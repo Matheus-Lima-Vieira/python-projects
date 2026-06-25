650a8bc8ee7ab2b1fbcf195387516e72b5c72d31
Matheus <matheuslsv8@gmail.com>
2026-06-25 17:26:09 -0300

    Finalizado Projeto 03 - Catálogo de Filmes

--- Projeto03_CatalogoFilmes/projeto_catalogo_filmes.py
+++ Projeto03_CatalogoFilmes/projeto_catalogo_filmes.py
@@ -8,22 +8,36 @@ while True:
     selecao = input("1 - Adicionar filme\n" \
                     "2 - Listar filmes\n" \
                     "3 - Sair\n")
-
+ 
     if selecao == "1":
-        opcao = input(f"=====Deseja adicionar um filme ao catálogo? Sim / Não=====\n")
-        while opcao == "Sim":
+        while True:
             nome_filme = input(f"Qual o nome do filme?\n")
             ano = int(input("Qual o ano do filme?\n"))
-            nota = input("Qual a nota do filme?\n")
-            opcao = input(f"Deseja adicionar mais um filme?\n")
+            nota = float(input("Qual a nota do filme?\n"))
             lista.append([nome_filme,ano,nota])
+            opcao = input(f"=====Deseja adicionar mais um filme ao catálogo? Sim / Não=====\n").strip().lower()
+            if opcao != "sim":
+                break
+
     elif selecao == "2":
-        print(lista)
-        break
+        print("\n===== CATÁLOGO DE FILMES =====")
+        if not lista:
+            print("Não há filmes cadastrados\n")
+        for filme in lista:
+            print((f"{filme[0]} ") + (f"({filme[1]})"))
+            print(f"Nota: {filme[2]}\n")
+        opccatalogo = input("1 - Retornar ao menu anterior\n"
+                            "2 - Sair\n")
+        if opccatalogo == "2":
+            print("\nObrigado por usar nosso catálogo, Até a próxima!\n" \
+              "Sistema encerrado...")
+            break
+
     elif selecao == "3":
-        print("\nObrigado por usar nosso catálogo, Até a próxima!")
-        print("Sistema encerrado...")
+        print("\nObrigado por usar nosso catálogo, Até a próxima!\n" \
+              "Sistema encerrado...")
         break
+
     else:
         print("Opção incorreta")
 
