1b5037c465ed2fe8bfba569da08220cb5072939b
Matheus <matheuslsv8@gmail.com>
2026-07-08 09:06:37 -0300

    Projeto 5: Agenda telefônica com busca, remoção e validações

--- Projeto05_AgendaTelefonica/agenda_telefonica.py
+++ Projeto05_AgendaTelefonica/agenda_telefonica.py
@@ -1,13 +1,14 @@
 import re
 
 # Lista para testes
+'''
 lista = [
      ["Matheus Lima", "11","99999-9999", "matheus@email.com"],
      ["João Silva", "11","98888-8888", "joao@email.com"],
      ["Maria Souza", "11","97777-7777", "maria@email.com"]
  ]
-
-#lista = []
+'''
+lista = []
 
 while True:
     print("====Agenda====")
@@ -21,25 +22,34 @@ while True:
         while True:
             print("====Agenda====")
             nome1 = input(f"Insira o nome do contato\n").strip().title()
-            nome2 = re.sub(r'[^a-zA-Zá-úÁ-Ú]', ' ', nome1)
+            nome2 = re.sub(r'[^a-zA-Zá-úÁ-Ú]', ' ', nome1).strip()
             #.title() metodo para a primeira letra de todas as palavras seja maiuscula e as demais minusculas
             #.capitalize() metodo para que a primeira letra seja maiuscula e as demais minusculas
             #re.sub para validar se o nome contem somente letras
             ddd1 = (input(f"Insira o DDD\n"))
             ddd2 = re.sub('[^0-9]', '', ddd1)
             #re.sub para tirar digitos que nao sejam numeros
+            #ddd = re.sub('[^0-9]', '', input("Insira o DDD\n")) FORMA MAIS LIMPA DE USAR
             if len(ddd2) == 2:
-                #ddd_formatado = ddd
                 numero1 = input("Insira o numero do telefone\n")
                 numero2 = re.sub('[^0-9]', '', numero1)
                 #re.sub para tirar digitos que nao sejam numeros
-                email = input(f"Insira o e-mail\n")
-                lista.append([nome2, ddd2, numero2, email])
-                opcao = input("Deseja adicionar mais um contato? Sim / Não\n").strip().lower()
-                if opcao != "sim":
-                    break
+                #numero = re.sub('[^0-9]', '', input("Insira o telefone\n")) FORMA MAIS LIMPA DE USAR
+                if len(numero2) == 9 and numero2.isdigit():
+                    numero_formatado = f"{numero2[0:5]}-{numero2[5:9]}"
+                    email = input(f"Insira o e-mail\n").strip().lower()
+                    if "@" in email and "." in email:
+                        lista.append([nome2, ddd2, numero_formatado, email])
+                        opcao = input("Deseja adicionar mais um contato? Sim / Não\n").strip().lower()
+                        if opcao != "sim":
+                            break
+                    else:
+                        print("E-mail inválido!")
+                else:
+                    print("Número inválido")
             else:
                 print("DDD está incorreto")
+        
     elif selecao == "2":
         print("====Agenda====")
         if not lista:
@@ -48,7 +58,7 @@ while True:
             for agenda in lista:
                 #print(lista)
                 print((f"Nome: {agenda[0]}\n") + 
-                      (f"Telefone: ({agenda[1]})") + (f"{agenda[2]}\n") + 
+                      (f"Telefone: ({agenda[1]}) ") + (f"{agenda[2]}\n") + 
                       (f"E-mail: {agenda[3]}\n"))
     elif selecao == "3":
         print("====Agenda====")
@@ -64,20 +74,18 @@ while True:
                 break
         if not busca_contato: 
                 print("Contato não encontrado")
-#===================================================================================================
     elif selecao == "4":
         print("====Agenda====")
-        remocao = input(f"Qual contato deseja apagar?\n")
+        remocao = input(f"Qual contato deseja apagar?\n").strip().title()
         remove_contato = False
         for agenda in lista:
             if remocao == agenda[0]:
                 remove_contato = True
-                #contato_removido = lista.remove(lista[0])
-                print(f"Contato {remocao} removido!\n")
+                lista.remove(agenda)
+                print(f"Contato {remocao} removido!")
                 break
         if not remove_contato: 
                 print("Contato não encontrado")
-#===================================================================================================
     elif selecao =="5":
         print("\nEncerrando a agenda telefonica")
         break
